fadd1fbdf413a740981dd90d35799b563509eab4
Christopher King <G.nius.ck@gmail.com>
2015-05-22 13:02:13 -0400

    Added array for ways to win

--- main.cpp
+++ main.cpp
@@ -126,6 +126,17 @@ void subSquare()
     cout << "Better luck next time " << name[loser] << ". :(" << endl;
 }
 
+int winningWays[8][3] = {
+    {0, 1, 2},
+    {4, 5, 6},
+    {7, 8, 9},
+    {0, 4, 7},
+    {1, 5, 8},
+    {2, 6, 9},
+    {0, 5, 9},
+    {7, 5, 2}
+                                    };
+
 void magicSquare()
 {
     string name;
